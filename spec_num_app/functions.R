3633b1cb183a12eee8fd5eb93297a571368f4c50
vjpsyverson <vsyverson@gmail.com>
2016-11-30 16:10:16 -0600

    squished NHM-NHMW bug
    Words are now split after known museum abbreviations UNLESS immediately
    followed by a capital letter. Also, in case something else weird
    happens with string splitting, an if() should now catch any
    abbreviations flagged by the initial SQL query that are missing from
    the actual sentence.

--- spec_num_app/functions.R
+++ spec_num_app/functions.R
@@ -11,51 +11,45 @@ extractSpecNos<-function(x){
   insts<-unlist(strsplit(as.character(x["inst"])," "))
   insts<-insts[order(nchar(insts),decreasing = T)]
   #acquire, clean, and split words
-  words<-strsplit(gsub(",,,",",COMMA,",gsub("[(\"){}]","",x["words"])),",")[[1]]
-  instPlusString<-paste0("(.*)(",insts[1],")(.+)")
-  instLoc<-which(grepl(instPlusString,words))
-  if(length(instLoc)>0) {words<-gsub(instPlusString,paste0("\\1","\\2",",","\\3"),words)}
-  for (k in 2:length(insts)){ #split words after institute abbreviations
-    instPlusString<-paste0("(.*)(",insts[k],")(.+)")
-    longerInstNames<-paste(insts[1:k][-k],collapse = "|")
-    instLoc<-which(!grepl(longerInstNames,words)&grepl(instPlusString,words))
+  words<-strsplit(gsub(",,,",",COMMA,",gsub("[(\"){}]","",x["words"])),",")[[1]] #remove punctuation rubble, clean up real commas
+  for (k in 1:length(insts)){ #split words after institute abbreviations unless followed by another capital letter
+    instPlusString<-paste0("(.*)(",insts[k],")([^A-Z]+)")
+    instLoc<-which(!grepl(longerInstNames,words))
     if(length(instLoc)>0) {words<-gsub(instPlusString,paste0("\\1","\\2",",","\\3"),words)}
   }
-  words<-unlist(strsplit(words,","))
-  words[which(words=="COMMA")]<-","
+  words<-unlist(strsplit(words,",")) #split on nlp352's inserted commas
+  words[which(words=="COMMA")]<-"," #re-insert real commas
   #extract tuples {docid, sentid, inst, specnos}
   inSentence<-data.frame(docid=numeric(),sentid=numeric(),inst=character(),specnos=character(),stringsAsFactors=F)
   for (k in 1:length(insts)){
     findAbbr<-grep(insts[k],words,fixed=T) #find all instances of this institution abbreviation
     speclocs<-specnos<-numeric() #zero speclocs and specnos for each institution abbreviation
-    for(j in 1:length(findAbbr)){ #catch all the number-like strings associated with each instance
-      start<-findAbbr[j]
-      end<-start+1
-      while (grepl("[[:digit:]]",words[end])==T|words[end]%in%fillers) {
-        if (words[end]%in%fillers) {end<-end+1}
-        if (grepl("[[:digit:]]",words[end])==T) {
-          speclocs<-c(speclocs,end)
-          end<-end+1
+    if (length(findAbbr)>0) {
+      for(j in 1:length(findAbbr)){ #catch all the number-like strings associated with each instance
+        start<-findAbbr[j]
+        end<-start+1
+        while (grepl("[[:digit:]]",words[end])==T|words[end]%in%fillers) {
+          if (words[end]%in%fillers) {end<-end+1}
+          if (grepl("[[:digit:]]",words[end])==T) {
+            speclocs<-c(speclocs,end)
+            end<-end+1
           }
         }
       }
+    }
     if(length(speclocs)>0){ #if any instance of this abbreviation is associated with any numbers
-      specnos<-words[speclocs][grepl("[[:digit:]]",words[speclocs])
-                               &!grepl("[^[:alnum:]][x\\+][0-9]+",words[speclocs])
-                               &!grepl("[[:digit:]]x[[:digit:]]",words[speclocs])] 
-                               #assign the ones with numbers in them that are not magnification numbers to specnos
+      specnos<-words[speclocs][grepl("[[:digit:]]",words[speclocs])] #assign the ones with numbers in them to specnos
     }
     if(length(specnos)>0){
-      specnos<-gsub("^-","",specnos) #strip initial hyphens
       inSentence[(nrow(inSentence)+1):(nrow(inSentence)+length(specnos)),]<-cbind(
         x["docid"],x["sentid"],rep(insts[k],length(specnos)),specnos,row.names=NULL,stringsAsFactors=F)
-      }
     }
+  }
   if (nrow(inSentence)>0) {result<-inSentence} else {
     result<-cbind(docid=x["docid"],sentid=x["sentid"],inst=NA_character_,specnos=NA_character_)
-    }
-  return(data.frame(result))
   }
+  return(data.frame(result))
+}
 ##wrapper applying extractSpecNos to table of sentences + "inst" column
 extractMultiple<-function(x){
   specList<-suppressWarnings(apply(x,1,extractSpecNos))
